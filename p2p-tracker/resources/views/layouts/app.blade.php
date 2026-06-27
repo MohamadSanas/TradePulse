97ea1d00a1d5fb17a8964d482aa4b5d8a08a1a43
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2026-06-27 21:30:44 +0530

    Replace @vite directive with manual Vite asset URLs for HTTPS debugging

--- p2p-tracker/resources/views/layouts/app.blade.php
+++ p2p-tracker/resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ Illuminate\Support\Facades\Vite::asset('resources/css/app.css') }}">
+        <script type="module" src="{{ Illuminate\Support\Facades\Vite::asset('resources/js/app.js') }}"></script>   
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
