1cce7349efab76636e43ce7a14d94c58e53c33e7
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2026-06-27 22:06:16 +0530

    Fix HTTPS asset URLs and add guest layout favicon

--- p2p-tracker/resources/views/layouts/app.blade.php
+++ p2p-tracker/resources/views/layouts/app.blade.php
@@ -13,8 +13,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        <link rel="stylesheet" href="{{ Illuminate\Support\Facades\Vite::asset('resources/css/app.css') }}">
-        <script type="module" src="{{ Illuminate\Support\Facades\Vite::asset('resources/js/app.js') }}"></script>   
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
