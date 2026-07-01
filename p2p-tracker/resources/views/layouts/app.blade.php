5bdbecc8dc7471d1029207f8f640f8080c5e802a
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2026-07-01 16:56:31 +0530

    Redesign authentication and dashboard UI

--- p2p-tracker/resources/views/layouts/app.blade.php
+++ p2p-tracker/resources/views/layouts/app.blade.php
@@ -11,21 +11,26 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        @stack('head')
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body class="bg-black font-sans antialiased">
+        <div class="min-h-screen bg-black">
+            @unless (request()->routeIs('dashboard'))
+                @include('layouts.navigation')
+            @endunless
 
             <!-- Page Heading -->
             @isset($header)
+                @unless (request()->routeIs('dashboard'))
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
+                @endunless
             @endisset
 
             <!-- Page Content -->
