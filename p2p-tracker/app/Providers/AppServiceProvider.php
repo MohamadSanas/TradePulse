9a45f1222044cc2c16660a15a52a441f86798d13
Mohroof Mohamad sanas <159630755+MohamadSanas@users.noreply.github.com>
2026-07-01 15:14:39 +0530

    fix: HTTPs assets URL in devlopment version

--- p2p-tracker/app/Providers/AppServiceProvider.php
+++ p2p-tracker/app/Providers/AppServiceProvider.php
@@ -23,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
+        else {
+            URL::forceScheme('http');
+        }
     }
 }
